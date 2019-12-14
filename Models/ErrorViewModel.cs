ddf06c83ffac439c3bc438303ac15d69feca4f15
Play_D <ranzeplay@gmail.com>
2019-12-14 16:32:13 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Semicolon.OnlineJudge.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
