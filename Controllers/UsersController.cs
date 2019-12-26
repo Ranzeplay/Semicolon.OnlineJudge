834bce122d3d74fbbc6e4fbf45705b64448c18a3
Play_D <ranzeplay@gmail.com>
2019-12-26 17:43:51 +0800

    Records page

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Semicolon.OnlineJudge.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index(string id)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ViewModels/Users/IndexModel.cs
@@ -0,0 +1,13 @@
+﻿using Semicolon.OnlineJudge.Models.Judge;
+using Semicolon.OnlineJudge.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Semicolon.OnlineJudge.Models.ViewModels.Users
+{
+    public class IndexModel
+    {
+    }
+}
