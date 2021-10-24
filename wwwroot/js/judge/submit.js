4424f16e193bd60d7b34fd6d8947117f26215415
Jeb Feng <ranzeplay@outlook.com>
2021-10-24 11:34:44 +0800

    Dynamically load all supported languages

--- wwwroot/js/judge/submit.js
+++ wwwroot/js/judge/submit.js
@@ -1,17 +1,24 @@
 ﻿var availableLanguages;
-
-
-// Do these on page loaded
 var editor = monaco.editor.create(document.getElementById('code'), {
     language: 'cpp',
     fontSize: "18px"
 });
 
-$.get("/api/supported_langs", function (result) {
-    availableLanguages = JSON.parse(result);
+$(document).ready(function () { loadLangOptions(); });
 
-    $("LanguageId").toggleClass("hidden");
-});
+function loadLangOptions() {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', '/api/supported_langs');
+
+    httpRequest.onreadystatechange = function () {
+        var json = JSON.parse(httpRequest.responseText);
+
+        console.log(json);
+        availableLanguages = json;
+    }
+
+    httpRequest.send();
+}
 
 function completeCode() {
     document.getElementById('code-saver').value = editor.getValue();
